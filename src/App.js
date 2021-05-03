01a6a9d67de05ec12b97d6944afe28a5e367964e
Ganesh <ganeshnnaik1997@gmail.com>
2021-05-03 08:02:30 +0530

    calculater first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Screen from './Screen';
+import './css/calculator.css';
+import Keypad from './Keypad';
+
+function App() {
+  return (
+    <div className='calculator'>
+        <Screen/>
+        <Keypad/>
+    </div>
+  );
+}
+
+export default App;
